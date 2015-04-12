e6132018e377330fd56d5e9a13db0ee315f104dd
raavr <rafal.r26@gmail.com>
2015-04-12 13:12:50 +0200

    modified constants, renamed main to validator

--- src/pl/rr/Constant.java
+++ src/pl/rr/Constant.java
@@ -9,16 +9,16 @@ import java.util.Map;
  * @author rykowskr
  */
 public class Constant {
-    public static final int XML_START = 256;// <?xml
-    public static final int XML_END = 257;// ?>
-    public static final int GREATERTHAN = 259;// >
-    public static final int SLASH = 260;// /
-    public static final int EQ = 261;// =
-    public static final int ANY_STRING = 263;// everything without <
-    public static final int ATTR_VALUE = 264; // " String without < "
-    public static final int LESSTHAN_SLASH_ID = 265; // </ String a-z A-Z 0-9 . - _ without digits at the beginning
-    public static final int LESSTHAN_ID = 266; // < String a-z A-Z 0-9 . - _  without digits at the beginning
-    public static final int ATTR_NAME = 268; // String without space and <
+    public static final int XML_START = 1;// <?xml
+    public static final int XML_END = 2;// ?>
+    public static final int GREATERTHAN = 3;// >
+    public static final int SLASH = 4;// /
+    public static final int EQ = 5;// =
+    public static final int ANY_STRING = 6;// everything without <
+    public static final int ATTR_VALUE = 7; // " String without < "
+    public static final int LESSTHAN_SLASH_ID = 8; // </ String a-z A-Z 0-9 . - _ without digits at the beginning
+    public static final int LESSTHAN_ID = 9; // < String a-z A-Z 0-9 . - _  without digits at the beginning
+    public static final int ATTR_NAME = 10; // String without space and <
     public static final int END = 11;
     public static final int ERROR = -1;
     public static final int TAG_NOT_EQUALS = -2;
@@ -27,17 +27,17 @@ public class Constant {
     //for error msgs
     static {
         HashMap<Integer, String> _hm = new HashMap<>();
-        _hm.put(256, "<?xml");
-        _hm.put(257, "?>");
-        _hm.put(259, "domknięcia '>'");
-        _hm.put(260, "ukośnika '/'");
-        _hm.put(261, "rowności '='");
-        _hm.put(263, "we wnętrzu taga");
-        _hm.put(264, "wartości atrybutu");
-        _hm.put(265, "id tagu zamykającego");
-        _hm.put(266, "id tagu otwierającego");
-        _hm.put(268, "rodzaju atybutu");
-        _hm.put(11, "końcu");
+        _hm.put(XML_START, "<?xml");
+        _hm.put(XML_END, "?>");
+        _hm.put(GREATERTHAN, "domknięcia '>'");
+        _hm.put(SLASH, "ukośnika '/'");
+        _hm.put(EQ, "rowności '='");
+        _hm.put(ANY_STRING, "we wnętrzu taga");
+        _hm.put(ATTR_VALUE, "wartości atrybutu");
+        _hm.put(LESSTHAN_SLASH_ID, "id tagu zamykającego");
+        _hm.put(LESSTHAN_ID, "id tagu otwierającego");
+        _hm.put(ATTR_NAME, "rodzaju atybutu");
+        _hm.put(END, "końcu");
         hm = Collections.unmodifiableMap(_hm);
     }
 
--- src/pl/rr/Validator.java
+++ src/pl/rr/Validator.java
@@ -1,6 +1,5 @@
 package pl.rr;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
